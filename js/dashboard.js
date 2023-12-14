f99d33b18acdc7efd459cd909bb42c4c33f8271b
Malek YAICH <mmyaich@gmail.com>
2023-12-14 13:33:45 +0100

    real time btc price

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,112 +1,134 @@
-(function($) {
-  'use strict';
-  $(function() {
-    if ($("#performaneLine").length) {
-      var graphGradient = document.getElementById("performaneLine").getContext('2d');
-      var graphGradient2 = document.getElementById("performaneLine").getContext('2d');
-      var saleGradientBg = graphGradient.createLinearGradient(5, 0, 5, 100);
-      saleGradientBg.addColorStop(0, 'rgba(26, 115, 232, 0.18)');
-      saleGradientBg.addColorStop(1, 'rgba(26, 115, 232, 0.02)');
-      var saleGradientBg2 = graphGradient2.createLinearGradient(100, 0, 50, 150);
-      saleGradientBg2.addColorStop(0, 'rgba(0, 208, 255, 0.19)');
-      saleGradientBg2.addColorStop(1, 'rgba(0, 208, 255, 0.03)');
-      var salesTopData = {
-          labels: ["SUN","sun", "MON", "mon", "TUE","tue", "WED", "wed", "THU", "thu", "FRI", "fri", "SAT"],
-          datasets: [{
-              label: 'This week',
-              data: [50, 110, 60, 290, 200, 115, 130, 170, 90, 210, 240, 280, 200],
-              backgroundColor: saleGradientBg,
-              borderColor: [
-                  '#1F3BB3',
-              ],
-              borderWidth: 1.5,
-              fill: true, // 3: no fill
-              pointBorderWidth: 1,
-              pointRadius: [4, 4, 4, 4, 4,4, 4, 4, 4, 4,4, 4, 4],
-              pointHoverRadius: [2, 2, 2, 2, 2,2, 2, 2, 2, 2,2, 2, 2],
-              pointBackgroundColor: ['#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3','#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3','#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3','#1F3BB3)'],
-              pointBorderColor: ['#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff',],
-          },{
-            label: 'Last week',
-            data: [30, 150, 190, 250, 120, 150, 130, 20, 30, 15, 40, 95, 180],
-            backgroundColor: saleGradientBg2,
-            borderColor: [
-                '#52CDFF',
-            ],
-            borderWidth: 1.5,
-            fill: true, // 3: no fill
-            pointBorderWidth: 1,
-            pointRadius: [0, 0, 0, 4, 0],
-            pointHoverRadius: [0, 0, 0, 2, 0],
-            pointBackgroundColor: ['#52CDFF)', '#52CDFF', '#52CDFF', '#52CDFF','#52CDFF)', '#52CDFF', '#52CDFF', '#52CDFF','#52CDFF)', '#52CDFF', '#52CDFF', '#52CDFF','#52CDFF)'],
-              pointBorderColor: ['#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff','#fff',],
-        }]
-      };
-  
-      var salesTopOptions = {
-        responsive: true,
-        maintainAspectRatio: false,
-          scales: {
-              yAxes: [{
-                  gridLines: {
-                      display: true,
-                      drawBorder: false,
-                      color:"#F0F0F0",
-                      zeroLineColor: '#F0F0F0',
-                  },
-                  ticks: {
-                    beginAtZero: false,
-                    autoSkip: true,
-                    maxTicksLimit: 4,
-                    fontSize: 10,
-                    color:"#6B778C"
-                  }
-              }],
-              xAxes: [{
-                gridLines: {
-                    display: false,
-                    drawBorder: false,
+
+
+
+(function ($) {
+    'use strict';
+
+
+    $(function () {
+
+
+        if ($("#performaneLine").length) {
+            var graphGradient = document.getElementById("performaneLine").getContext('2d');
+            var saleGradientBg = graphGradient.createLinearGradient(5, 0, 5, 100);
+            saleGradientBg.addColorStop(0, 'rgba(26, 115, 232, 0.18)');
+            saleGradientBg.addColorStop(1, 'rgba(26, 115, 232, 0.02)');
+
+            // Initialisation des données du graphique avec des valeurs vides
+            var salesTopData = {
+                labels: [],  // Les données seront mises à jour dynamiquement
+                datasets: [{
+                    label: 'Bitcoin Price (USD)',
+                    data: [],  // Les données seront mises à jour dynamiquement
+                    backgroundColor: saleGradientBg,
+                    borderColor: ['#1F3BB3'],
+                    borderWidth: 1.5,
+                    fill: true,
+                    pointBorderWidth: 1,
+                    pointRadius: [4, 4, 4, 4, 4, 4, 4],
+                    pointHoverRadius: [2, 2, 2, 2, 2, 2, 2],
+                    pointBackgroundColor: ['#1F3BB3)', '#1F3BB3', '#1F3BB3', '#1F3BB3', '#1F3BB3)', '#1F3BB3', '#1F3BB3'],
+                    pointBorderColor: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff'],
+                }]
+            };
+
+            var salesTopOptions = {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            display: true,
+                            drawBorder: false,
+                            color: "#F0F0F0",
+                            zeroLineColor: '#F0F0F0',
+                        },
+                        ticks: {
+                            beginAtZero: false,
+                            autoSkip: true,
+                            maxTicksLimit: 4,
+                            fontSize: 10,
+                            color: "#6B778C"
+                        }
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false,
+                            drawBorder: false,
+                        },
+                        ticks: {
+                            beginAtZero: false,
+                            autoSkip: true,
+                            maxTicksLimit: 7,
+                            fontSize: 10,
+                            color: "#6B778C"
+                        }
+                    }],
                 },
-                ticks: {
-                  beginAtZero: false,
-                  autoSkip: true,
-                  maxTicksLimit: 7,
-                  fontSize: 10,
-                  color:"#6B778C"
+                legend: false,
+                legendCallback: function (chart) {
+                    var text = [];
+                    text.push('<div class="chartjs-legend"><ul>');
+                    for (var i = 0; i < chart.data.datasets.length; i++) {
+                        console.log(chart.data.datasets[i]); // see what's inside the obj.
+                        text.push('<li>');
+                        text.push('<span style="background-color:' + chart.data.datasets[i].borderColor + '">' + '</span>');
+                        text.push(chart.data.datasets[i].label);
+                        text.push('</li>');
+                    }
+                    text.push('</ul></div>');
+                    return text.join("");
+                },
+                elements: {
+                    line: {
+                        tension: 0.4,
+                    }
+                },
+                tooltips: {
+                    backgroundColor: 'rgba(31, 59, 179, 1)',
                 }
-            }],
-          },
-          legend:false,
-          legendCallback: function (chart) {
-            var text = [];
-            text.push('<div class="chartjs-legend"><ul>');
-            for (var i = 0; i < chart.data.datasets.length; i++) {
-              console.log(chart.data.datasets[i]); // see what's inside the obj.
-              text.push('<li>');
-              text.push('<span style="background-color:' + chart.data.datasets[i].borderColor + '">' + '</span>');
-              text.push(chart.data.datasets[i].label);
-              text.push('</li>');
+            };
+
+            var salesTop = new Chart(graphGradient, {
+                type: 'line',
+                data: salesTopData,
+                options: salesTopOptions
+            });
+
+            document.getElementById('performance-line-legend').innerHTML = salesTop.generateLegend();
+
+            // Fonction pour récupérer les données de l'API CoinGecko
+            async function fetchCryptoData() {
+                try {
+                    const symbol = "bitcoin";
+                    const currency = "usd";
+                    const days = 1;
+
+                    const cryptoData = await getHistoricalData(symbol, currency, days);
+
+                    if (cryptoData !== null) {
+                        // Mettez à jour les données du graphique
+                        salesTop.data.labels = cryptoData.map(entry => moment(entry.timestamp).format('HH:mm'));
+                        salesTop.data.datasets[0].data = cryptoData.map(entry => entry.price);
+
+                        // Mettez à jour le graphique
+                        salesTop.update();
+                    }
+                } catch (error) {
+                    console.error('Erreur lors de la récupération des données de l\'API CoinGecko:', error);
+                }
             }
-            text.push('</ul></div>');
-            return text.join("");
-          },
-          
-          elements: {
-              line: {
-                  tension: 0.4,
-              }
-          },
-          tooltips: {
-              backgroundColor: 'rgba(31, 59, 179, 1)',
-          }
-      }
-      var salesTop = new Chart(graphGradient, {
-          type: 'line',
-          data: salesTopData,
-          options: salesTopOptions
-      });
-      document.getElementById('performance-line-legend').innerHTML = salesTop.generateLegend();
-    }
+
+            // Appelez la fonction pour récupérer les données au chargement de la page
+            fetchCryptoData();
+
+            // Mettez en place une minuterie pour mettre à jour les données toutes les X secondes (par exemple, toutes les 60 secondes)
+            setInterval(fetchCryptoData, 60000);
+        }
+
+
+
+
     if ($("#performaneLine-dark").length) {
       var graphGradient = document.getElementById("performaneLine-dark").getContext('2d');
       var graphGradient2 = document.getElementById("performaneLine-dark").getContext('2d');
@@ -213,7 +235,15 @@
       });
       document.getElementById('performance-line-legend-dark').innerHTML = salesTopDark.generateLegend();
     }
-    if ($("#datepicker-popup").length) {
+
+
+
+
+
+
+
+
+      if ($("#datepicker-popup").length) {
       $('#datepicker-popup').datepicker({
         enableOnReadonly: true,
         todayHighlight: true,
@@ -773,3 +803,5 @@
   
   });
 })(jQuery);
+
+
